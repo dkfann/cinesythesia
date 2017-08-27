f54b2dbef64d036175307d448810b3572d9a9209
dkfann <danny.k.fann@gmail.com>
2017-08-27 03:01:43 -0700

    Added basic styling for movie search UI

--- stores/searchStore.js
+++ stores/searchStore.js
@@ -4,11 +4,11 @@ import { TMDB_API_KEY, OMDB_API_KEY } from '../dev-private/apiKeys';
 
 class SearchStore {
     @observable query;
-    @observable movieData;
+    @observable movieSearchData;
 
     constructor(query = 'test') {
         this.query = query;
-        this.movieData = [];
+        this.movieSearchData = [];
     }
     @computed get currentQuery() {
         return this.query;
@@ -19,11 +19,12 @@ class SearchStore {
     @action searchTMDBforMovieByName = query => {
         const movieSearcher = movieApi({ tmdbApiKey: TMDB_API_KEY, omdbApiKey: OMDB_API_KEY });
 
-        const movieQuery = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1000&vote_average.gte=8`;
+        // const movieQuery = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1000&vote_average.gte=8`;
+        const movieQuery = `https://api.themoviedb.org/3/search/movie/?api_key=${TMDB_API_KEY}&query=${query}`;
 
         movieSearcher.getMovieTitleQueryFromTMDBQuery(movieQuery)
             .then(resolve => {
-                this.movieData = resolve.results;
+                this.movieSearchData = resolve.results;
             });
     }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,44 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-	template: __dirname + '/app/index.html',
-	fielanem: 'index.html',
-	inject: 'body',
+    template: __dirname + '/app/index.html',
+    fielanem: 'index.html',
+    inject: 'body',
 });
 
 module.exports = {
-	devtool: 'source-map',
-	entry: [
-		'./app/index.js',
-	],
-	output: {
-		path: __dirname + '/dist',
-		filename: 'index_bundle.js',
-	},
-	module: {
-		loaders: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-		]
-	},
-	plugins: [HTMLWebpackPluginConfig],
+    devtool: 'source-map',
+    entry: [
+        './app/index.js',
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'index_bundle.js',
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+            {
+                test: /\.css$/,
+                loader: 'style-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                    localIndentName: '[name]__[local]___[hash:base64:5]',
+                },
+            },
+            // {
+            //   test: /\.css$/,
+            //   loaders: [
+            //     'style',
+            //     'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&sourceMap&-minimize'
+            //   ]
+            // },
+        ]
+    },
+    plugins: [
+        HTMLWebpackPluginConfig,
+    ],
 }
